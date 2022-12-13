ae3e548907822fb69f7188a518be3561e2dc3672
TheMode <themode@outlook.fr>
2022-12-13 19:47:13 +0100

    Add binary expr

--- src/main/java/org/click/Expression.java
+++ src/main/java/org/click/Expression.java
@@ -23,4 +23,17 @@ public sealed interface Expression {
 
     record Range(Expression start, Expression end, Expression step) implements Expression {
     }
+
+    record Binary(Expression left, Operator operator, Expression right) implements Expression {
+    }
+
+    enum Operator {
+        ADD, SUBTRACT,
+        MULTIPLY, DIVIDE,
+        MODULO,
+        EQUAL, NOT_EQUAL,
+        LESS_THAN, LESS_THAN_OR_EQUAL,
+        GREATER_THAN, GREATER_THAN_OR_EQUAL,
+        AND, OR
+    }
 }
